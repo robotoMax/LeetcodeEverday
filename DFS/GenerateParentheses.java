7f909d0a9692005ccad0473d7e4a9b22feccf5bd
robotoMax <robotoMax@github.com>
2018-03-06 00:43:49 -0500

    Fighting

--- DFS/GenerateParentheses.java
+++ DFS/GenerateParentheses.java
@@ -16,6 +16,30 @@
  * ]
  * 
  */
+import java.util.*;
 public class GenerateParentheses {
-
+    public List<String> solution(int n) {
+        List<String> res = new ArrayList<>();
+        if (n == 0) return res;
+        StringBuilder sb = new StringBuilder();
+        helper(n, res, 0, 0, sb);
+        return res;
+    }
+    public void helper(int n, List<String> res, int left, int right, StringBuilder sb) {
+        if (sb.length() == 2 * n) {
+            res.add(sb.toString());
+            return;
+        }
+        if (left > right) {
+            sb.append(")");
+            helper(n, res, left, right + 1, sb);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+        if (left < n) {
+            sb.append("(");
+            helper(n, res, left + 1, right, sb);
+            sb.deleteCharAt(sb.length() - 1);            
+        }
+        return;
+    }
 }
