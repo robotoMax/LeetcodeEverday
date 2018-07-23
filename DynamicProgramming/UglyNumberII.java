4bdcddc242b0a25799d263eb5413bcb3c41ea05b
Stuart <stuart@Stuarts-MacBook-Pro.local>
2018-07-22 21:52:43 -0700

    +1

--- DynamicProgramming/UglyNumberII.java
+++ DynamicProgramming/UglyNumberII.java
@@ -12,16 +12,19 @@
  */
 public class UglyNumberII {
     public int nthUglyNumber(int n) {
-        int[] nums = new int[n + 1];
-        nums[1] = 1;
-        int u2 = nums[1], u3 = nums[1], u5 = nums[1];
-        int i2 = 0, i3 = 0, i5 = 0;
-        for (int i = 1; i <= n; i++) {
-            nums[i] = Math.min(u2, Math.min(u3, u5));
-            if (nums[i] == u2) u2 = nums[++i2] * 2;
-            if (nums[i] == u3) u3 = nums[++i3] * 3;
-            if (nums[i] == u5) u5 = nums[++i5] * 5;
+        int[] ugly = new int[n];
+        int next2 = 1;
+        int next3 = 1;
+        int next5 = 1;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        for (int i = 0; i < n; i++) {
+            ugly[i] = Math.min(next2, Math.min(next3, next5));
+            if (ugly[i] == next2) next2 = ugly[index2++] * 2;
+            if (ugly[i] == next3) next3 = ugly[index3++] * 3;
+            if (ugly[i] == next5) next5 = ugly[index5++] * 5;
         }
-        return nums[n];
+        return ugly[n - 1];
     }
 }
